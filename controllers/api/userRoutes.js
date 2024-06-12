6128d4dc32bfcd420b8c091717604a43afd0990b
B Water <brennw47@gmail.com>
2024-06-11 21:15:03 -0500

    feat(CSS) CSS fix.
    feat(CSS) CSS fix.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -21,6 +21,23 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.get("/signup", async (req, res) => {
+  try {
+    res.render("signup");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/signup", async (req, res) => {
+  try {
+    const userData = await Users.create(req.body);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 // // Example route using Axios to fetch data from an external API
 // router.get('/example', async (req, res) => {
 //   try {
